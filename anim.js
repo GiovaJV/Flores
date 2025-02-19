d6d4c9f4fff35ec14ff923ebad5bbe2cc60a255c
GiovaJV <165813093+GiovaJV@users.noreply.github.com>
2025-02-19 17:18:27 -0400

    Update anim.js

--- anim.js
+++ anim.js
@@ -36,7 +36,7 @@ var lyricsData = [
 function updateLyrics() {
   var time = Math.floor(audio.currentTime);
   var currentLine = lyricsData.find(
-    (line) => time >= line.time && time < line.time + 3
+    (line) => time >= line.time && time < line.time + 6
   );
 
   if (currentLine) {
